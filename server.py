6cc5a2a38dcd3a735bddc6695dedbf65378fee02
irulez22 <jsypoer@Gmail.com>
2026-09-26 02:17:43 +0200

    v10.2: update server version

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import asyncio,json,os,subprocess
 from pathlib import Path
 from aiohttp import web,WSMsgType
-ROOT=Path(__file__).resolve().parent;WEB=ROOT/'web';VERSION='v10.1'
+ROOT=Path(__file__).resolve().parent;WEB=ROOT/'web';VERSION='v10.2'
 clients={'display':set(),'admin':set()}
 gpio_buttons=[]
 
